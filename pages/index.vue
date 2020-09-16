557769a871f19a6025a748e7b377dced0b32a260
kensuke goto <kensuke.goto.1981@gmail.com>
2020-09-16 13:03:40 +0900

    wowを実装

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container wow fadeInUp">
+    <div class="block block--a"></div>
+    <div class="block block--b"></div>
+    <div class="block block--c"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted(){
+    new WOW().init();
+  }
+}
+</script>
+
+<style>
+.wow {
+  visibility: hidden;
+}
+.block{
+  height: 500px;
+
+}
+.block--a{
+  background-color: yellow;
+}
+.block--b{
+  background-color: purple;
+}
+.block--c{
+  background-color: pink;
+}
+</style>
